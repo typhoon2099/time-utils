7dbd5fd7ede0144ba2fac8ec29b2233867a40ecb
Matthew Simpson <matthew@matthewsimpson.net>
2021-12-23 22:02:27 +0000

    Add DurationFormatter.clock method

--- packages/duration-formatter/src/duration-formatter.js
+++ packages/duration-formatter/src/duration-formatter.js
@@ -1,6 +1,8 @@
-import { Duration } from '@time-utils/duration';
-
 export default class DurationFormatter {
+  static #SECONDS_PER_MINUTE = 60;
+  static #MINUTES_PER_HOUR = 60;
+  static #HOURS_PER_DAY = 24;
+
   /**
    * @param {Duration} duration
    */
@@ -8,7 +10,36 @@ export default class DurationFormatter {
     this.duration = duration;
   }
 
-  template(string) {
-    return string.replace('%l', this.duration.milliseconds);
+  static #second(seconds) {
+    return seconds % DurationFormatter.#SECONDS_PER_MINUTE;
+  }
+
+  static #minute(minute) {
+    return minute % DurationFormatter.#MINUTES_PER_HOUR;
+  }
+
+  static #hour(hours) {
+    return hours % DurationFormatter.#HOURS_PER_DAY;
+  }
+
+  clock() {
+    let negative = false;
+    const { hours, minutes, seconds } = this.duration;
+
+    if (this.duration.milliseconds < 0) {
+      negative = true;
+    }
+
+    let parts = [
+      DurationFormatter.#hour(Math.abs(hours)),
+      DurationFormatter.#minute(Math.abs(minutes)),
+      DurationFormatter.#second(Math.abs(seconds)),
+    ];
+
+    while (parts[0] === 0) {
+      parts.shift();
+    }
+
+    return `${negative ? '-' : ''}${parts.map(part => part.toString().padStart(2, '0')).join(':')}`;
   }
 }
